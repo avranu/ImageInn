fbcab4205d398ae197f57370c11d8cd5f403174c
Avranu <jess.a.mann@gmail.com>
2023-08-23 16:09:45 -0400

    implement base run

--- scripts/import_sd/providers/merge/base.py
+++ scripts/import_sd/providers/merge/base.py
@@ -19,14 +19,47 @@
 """
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import List, Dict, Any, Union, Optional
+from typing import List, Dict, Any, TypeVar, Union, Optional
+import logging
 from scripts.lib.path import FilePath
 from scripts.import_sd.providers.base import Provider
 from scripts.import_sd.photo import Photo
 
+logger = logging.getLogger(__name__)
+
+# Generic type that can be list[Photo] or Photo. This allows us to enforce the return type of a method matches its parameter
+BracketOrPhoto = TypeVar('BracketOrPhoto', List[Photo], Photo)
+
 class HDRProvider(Provider, ABC):
 
-	@abstractmethod
+	def run(self, brackets: list[BracketOrPhoto]) -> list[BracketOrPhoto]:
+		"""
+		Align the images in each bracket with the other images in the same bracket.
+
+		Args:
+			photos (list[Photo] | list[list[Photo]]): The photos to align.
+
+		Returns:
+			list[Photo] | list[list[Photo]]: 
+				The aligned photos. 
+				If any of the photos cannot be aligned within a braket, an empty list will be returned for that bracket.
+		"""
+		if len(brackets) < 1:
+			logger.debug('No brackets to align.')
+			return []
+		
+		# Handle just one bracket and return a list of photos
+		if isinstance(brackets[0], Photo):
+			return self.next(brackets)
+
+		# Handle a group of brackets and return a list of lists of photos
+		results = []
+		for bracket in brackets:
+			aligned_bracket = self.next(bracket)
+			results.append(aligned_bracket)
+
+		return results
+
 	def run(self, photos: list[Photo]) -> Photo | None:
 		"""
 		Merge a single bracket into an HDR image.
