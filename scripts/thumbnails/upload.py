c696342a309d4e5080a90fc63b3e855fa71fe004
Jess Mann <jess.a.mann@gmail.com>
2024-09-20 10:48:10 -0400

    improvements

--- scripts/thumbnails/upload.py
+++ scripts/thumbnails/upload.py
@@ -11,95 +11,159 @@ Example:
     >>> upload
 
 TODO:
-    Status report (txt file) for resuming progress
-    Pydantic
+    Suppress immich output
+    progress bar
+    concurrency
     
 """
 from __future__ import annotations
-from typing import Union
 import subprocess
 import os
 import logging
 from pathlib import Path
 from dotenv import load_dotenv
 import argparse
+from pydantic import BaseModel, Field, PrivateAttr, field_validator
+from typing import Iterable, List, Union, Optional
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Type: list[str | Path] | str | Path | None
-PathParts = Union[list[str | Path], str, Path]
+class AuthenticationError(Exception):
+    pass
 
-class Immich:
-    url : str
-    api_key : str
-    thumbnails_dir : Path
-    _ignore_extensions : list[str]
-    _ignore_paths : list[str]
-    _authenticated : bool = False
+class Immich(BaseModel):
+    url: str
+    api_key: str
+    directory: Path
+    ignore_extensions: List[str] = Field(default_factory=list)
+    ignore_paths: List[str] = Field(default_factory=list)
+    large_file_size: int = 1024 * 1024 * 100  # 100 MB
     
-    def __init__(self, url: str, api_key: str, thumbnails_dir : Path | str, ignore_extensions : list[str] | None = None, ignore_paths : PathParts | None = None):
-        self.url = url
-        self.api_key = api_key
-        self.thumbnails_dir = Path(thumbnails_dir)
-        self.ignore_extensions = ignore_extensions or []
-        self.ignore_paths = ignore_paths or []
+    _authenticated: bool = PrivateAttr(default=False)
 
-        if not self.thumbnails_dir.exists():
-            logger.error(f"Thumbnails directory {self.thumbnails_dir} does not exist.")
-            raise FileNotFoundError
+    @field_validator('directory', mode="before")
+    def validate_directory(cls, v):
+        # Allow str and list[str]
+        v = Path(v)
+        if not v.exists():
+            logger.error(f"Thumbnails directory {v} does not exist.")
+            raise FileNotFoundError(f"Thumbnails directory {v} does not exist.")
+        return v
 
-    @property
-    def ignore_extensions(self) -> list[str]:
-        return self._ignore_extensions
+    @field_validator('ignore_extensions', mode="before")
+    def validate_ignore_extensions(cls, v):
+        if not v:
+            return []
+        if isinstance(v, str):
+            return [v]
+        if isinstance(v, list):
+            return v
+        raise ValueError("Invalid ignore_extensions value.")
 
-    @ignore_extensions.setter
-    def ignore_extensions(self, value: list[str] | str | None):
-        if not value:
-            self._ignore_extensions = []
-            return
-
-        if isinstance(value, str):
-            self._ignore_extensions = [value]
-            return
-        
-        self._ignore_extensions = value
-
-    @property
-    def ignore_paths(self) -> list[str]:
-        return self._ignore_paths
-
-    @ignore_paths.setter
-    def ignore_paths(self, value: PathParts | None):
-        if not value:
-            self._ignore_paths = []
-            return
-
-        if isinstance(value, Path):
-            self._ignore_paths = [str(value)]
-            return
-        
-        if isinstance(value, str):
-            self._ignore_paths = [value]
-            return
-        
-        self._ignore_paths = [str(path) for path in value]
+    @field_validator('ignore_paths', mode="before")
+    def validate_ignore_paths(cls, v):
+        if not v:
+            return []
+        if isinstance(v, (str, Path)):
+            return [str(v)]
+        if isinstance(v, Iterable):
+            return [str(path) for path in v]
+        raise ValueError("Invalid ignore_paths value.")
 
     def authenticate(self):
         if self._authenticated:
             return
-        
         try:
             subprocess.run(["immich", "login-key", self.url, self.api_key], check=True)
             self._authenticated = True
             logger.info("Authenticated successfully.")
         except subprocess.CalledProcessError as e:
             logger.error(f"Authentication failed: {e}")
-            raise
+            raise AuthenticationError("Authentication failed.") from e
+
+    def should_ignore_file(self, file: Path) -> bool:
+        if file.suffix.lstrip('.').lower() in self.ignore_extensions:
+            logger.info(f"Ignoring file {file} due to extension.")
+            return True
+        if str(file) in self.ignore_paths:
+            logger.info(f"Ignoring file {file} due to path.")
+            return True
+        if file.stat().st_size > self.large_file_size:
+            logger.warning(f"File {file} is larger than {self.large_file_size} bytes and will be skipped.")
+            return True
+        return False
+
+    def load_status_file(self, directory: Path | None = None) -> dict[str, str]:
+        directory = directory or self.directory
+        status_file = directory / 'upload_status.txt'
+        status = {}
+        if status_file.exists():
+            with status_file.open('r') as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        parts = line.split('\t')
+                        if len(parts) == 2:
+                            filename, file_status = parts
+                            status[filename] = file_status
+
+        success = len([s for s in status.values() if s == 'success'])
+        failure = len([s for s in status.values() if s == 'failed'])
+        logger.info(f"Loaded status file {status_file}. Success: {success}, Failure: {failure}")
+                            
+        return status
+
+    def save_status_file(self, directory: Path, status: dict[str, str]):
+        status_file = directory / 'upload_status.txt'
+        logger.debug(f"Saving status file {status_file}")
+        with status_file.open('w') as f:
+            for filename, file_status in status.items():
+                f.write(f'{filename}\t{file_status}\n')
+
+    def upload_file(self, file: Path) -> bool:
+        command = ["immich", "upload", file.as_posix()]
+        try:
+            subprocess.run(command, check=True)
+            logger.info(f"Uploaded {file}")
+            return True
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Failed to upload {file}: {e}")
+            return False
+
+    def upload_files(self, recursive: bool = False):
+        if not self._authenticated:
+            self.authenticate()
+
+        directories = [self.directory]
+        if recursive:
+            directories.extend([d for d in self.directory.rglob('*') if d.is_dir()])
+
+        for directory in directories:
+            status = self.load_status_file(directory)
+            try:
+                for file in directory.iterdir():
+                    if not file.is_file():
+                        continue
+
+                    filename = file.name
+                    if filename in status and status[filename] == 'success':
+                        logger.debug(f"Skipping already uploaded file {file}")
+                        continue
+                    if self.should_ignore_file(file):
+                        continue
+
+                    success = self.upload_file(file)
+                    status[filename] = 'success' if success else 'failed'
+            finally:
+                self.save_status_file(directory, status)
 
     def find_large_files(self, directory : Path | None = None, size : int = 1024 * 1024 * 100) -> list[Path]:
+        """
+        Only used for upload_directory()
+        """
         if not directory:
-            directory = self.thumbnails_dir
+            directory = self.directory
             
         large_files = []
         for file in directory.rglob("**/*"):
@@ -108,6 +172,9 @@ class Immich:
         return large_files
 
     def _compile_ignore_patterns(self, directory : Path) -> list[str]:
+        """
+        Only used for upload_directory()
+        """
         ignore_patterns = []
         
         for ext in self.ignore_extensions:
@@ -126,11 +193,17 @@ class Immich:
 
         return ignore_patterns
 
-    def upload_files(self, recursive: bool = True, directory : Path | None = None):
+    def upload_directory(self, directory : Path | None = None, recursive : bool = True):
+        """
+        Not used, in favor of upload_files()
+
+        This method uses immich to upload the entire directory. However, re-compiling hashes for
+        every file is time consuming, and progress can't be resumed if terminated early. 
+        """
         if not self._authenticated:
             self.authenticate()
             
-        directory = directory or self.thumbnails_dir
+        directory = directory or self.directory
 
         command = ["immich", "upload"]
 
@@ -151,29 +224,40 @@ class Immich:
             logger.error(f"File upload failed: {e}")
 
 def main():
-    # Load environment variables from .env file
     load_dotenv()
-    
+
     url = os.getenv("IMMICH_URL")
     api_key = os.getenv("IMMICH_API_KEY")
     thumbnails_dir = os.getenv("CLOUD_THUMBNAILS_DIR")
 
     try:
-        parser = argparse.ArgumentParser(description="Upload JPG files to Immich.")
+        parser = argparse.ArgumentParser(description="Upload files to Immich.")
         parser.add_argument("--url", help="Immich URL", default=url)
         parser.add_argument("--api-key", help="Immich API key", default=api_key)
         parser.add_argument("--thumbnails-dir", '-d', help="Cloud thumbnails directory", default=thumbnails_dir)
         parser.add_argument("--ignore-extensions", "-e", help="Ignore files with these extensions", nargs='+')
         parser.add_argument('--ignore-paths', '-i', help="Ignore files with these paths", nargs='+')
+        parser.add_argument('--group', '-g', action='store_true', help="Upload files as a group via immich. Kept for legacy reasons, or if immich optimizes their recursive upload feature.")
         args = parser.parse_args()
 
         if not args.url or not args.api_key or not args.thumbnails_dir:
             logger.error("IMMICH_URL, IMMICH_API_KEY, and CLOUD_THUMBNAILS_DIR must be set.")
             exit(1)
 
-        immich = Immich(args.url, args.api_key, args.thumbnails_dir, args.ignore_extensions, args.ignore_paths)
-        immich.authenticate()
-        immich.upload_files()
+        immich = Immich(
+            url=args.url,
+            api_key=args.api_key,
+            directory=Path(args.thumbnails_dir),
+            ignore_extensions=args.ignore_extensions,
+            ignore_paths=args.ignore_paths
+        )
+
+        if args.group:
+            immich.upload_directory()
+        else:
+            immich.upload_files()
+    except AuthenticationError:
+        logger.error("Authentication failed. Check your API key and URL.")
     except KeyboardInterrupt:
         logger.info("Upload cancelled by user.")
 
