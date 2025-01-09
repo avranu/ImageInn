933e57b2d47efa532840d379e7d0c80f53b5e325
Jess Mann <jess.a.mann@gmail.com>
2025-01-09 17:12:29 -0500

    attempt fix

--- setup.py
+++ setup.py
@@ -10,19 +10,18 @@
 *        File:    setup.py                                                                                             *
 *        Project: imageinn                                                                                             *
 *        Version: 0.1.0                                                                                                *
-*        Created: 2024-10-08                                                                                           *
+*        Created: 2025-01-09                                                                                           *
 *        Author:  Jess Mann                                                                                            *
 *        Email:   jess.a.mann@gmail.com                                                                                *
-*        Copyright (c) 2024 Jess Mann                                                                                  *
+*        Copyright (c) 2025 Jess Mann                                                                                  *
 *                                                                                                                      *
 * -------------------------------------------------------------------------------------------------------------------- *
 *                                                                                                                      *
 *    LAST MODIFIED:                                                                                                    *
 *                                                                                                                      *
-*        2024-12-29     By Jess Mann                                                                                   *
+*        2025-01-09     By Jess Mann                                                                                   *
 *                                                                                                                      *
 *********************************************************************************************************************"""
 from setuptools import setup
 
 setup()
-
